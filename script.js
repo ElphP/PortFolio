0007646fdbf170cd4cac44f1ede8effa82abdcc5
Elphège <elphege78@gmail.com>
2022-08-20 23:50:45 +0200

    Typewriter V2

--- script.js
+++ script.js
@@ -1,93 +1,62 @@
-const animText1 = document.querySelector("#animText1");
-const animText2 = document.querySelector("#animText2");
-const animText3 = document.querySelector("#animText3");
+// création d'une table de valeur aléatoire pour les tempos entree lettres
+var tab = [];
+for (i = 0; i < 54; i++) {
+    tab[i] = Math.floor(Math.random() * 120 + 50);
+}
+// temps "d'arrêts"
+tab[6] = 580;
+tab[21] = 550;
+tab[29] = 320;
+tab[37] = 920;
+tab[54] = 850;
+var i = 0;
 
+const text1 = "Salut, c'est L-FÈJ.P,";
+const text2 = "je suis ";
+const text3 = "musicien";
+const text4 = "développeur web!";
+var cond2 = text1.length + text2.length;
+var cond3 = text1.length + text2.length + text3.length;
 
-
-const txt = "Salut, c'est L-FÈJ.P";
-const txt2 = "Je suis ";
-const txt3 = "musicien";
-const txt4 = "développeur web!";
-
-
-var tab = [
-    150, 200, 120, 190, 150, 250, 950, 130, 80, 140, 50, 120, 110, 120, 55, 312,
-    260, 386, 120, 135, 168,
-];
-var tab2 = [
-    50, 90, 120, 85, 50, 80, 150, 130, 92, 40, 150, 60, 110, 120, 155,
-    312, 260, 386, 120, 135, 168,
-];
-
-function typewriter(word, index) {
-    if (index < word.length) {
-        setTimeout(() => {
-            animText1.innerHTML += `<span>${word[index]}</span>`;
-            typewriter(txt, index + 1);
-        }, tab[index]);
-    }
-    else  {
-        setTimeout(() => {
-            typewriter2(txt2, 0);
-            }, 600);
+function type() {
+    if (i < text1.length) {
+        document.getElementById("animText1").innerHTML += text1.charAt(i);
+        i++;
+        setTimeout(type, tab[i]);
+    } else if (i < cond2) {
+        document.getElementById("animText2").innerHTML += text2.charAt(
+            i - text1.length
+        );
+        i++;
+        setTimeout(type, tab[i]);
+    } else if (i < cond3) {
+        document.getElementById("animText3").innerHTML += text3.charAt(
+            i - (text1.length + text2.length)
+        );
+        i++;
+        setTimeout(type, tab[i]);
+    } else if (i == cond3) {
+        document.getElementById("animText3").style.textDecoration =
+            "line-through";
+        setTimeout(type, 1200);
+        i++;
+    } else if (i == cond3 + 1) {
+        setTimeout(reset, 400);
+        setTimeout(type, 550);
+        i++;
+    } else if (i < cond3 + text4.length + 2) {
+        document.getElementById("animText3").innerHTML += text4.charAt(
+            i - (text1.length + text2.length + text3.length + 2)
+        );
+        i++;
+        setTimeout(type, tab[i]);
     }
 }
-setTimeout(() => {
-    typewriter(txt, 0);
-}, 200);
 
-
-
-function typewriter2(word, index) {
-    if (index < word.length) {
-        setTimeout(() => {
-            animText2.innerHTML += `<span>${word[index]}</span>`;
-            typewriter2(txt2, index + 1);
-        }, tab2[index]);
-    }
-    else  {
-       setTimeout(() => {
-            typewriter3(txt3, 0);
-            }, 500); 
-    }
-    }
-
-function typewriter3(word, index,text4) {
-    if (index < word.length) {
-        setTimeout(() => {
-            animText3.innerHTML += `<span>${word[index]}</span>`;
-            typewriter3(txt3, index + 1);
-        }, tab2[index]);
-    }
-        else  {
-            setTimeout(() => {
-                erase();
-            }, 500); 
-        } 
+function reset() {
+    document.getElementById("animText3").innerHTML = "";
+    document.getElementById("animText3").style.textDecoration = "none";
+    document.getElementById("animText3").style.color = "red";
 }
 
-function erase() {
-    animText3.style.textDecoration = "line-through";
- setTimeout(() => {
-     Maj();
- }, 500); 
-
-    
-}
-
-function Maj()  {
-    animText3.style.textDecoration = "none";
-    animText3.innerHTML = "";
-    animText3.style.color = "red";
-    typewriter4(txt4, 0);
-}
-
-function typewriter4(word, index) {
-     
-     if (index < word.length) {
-         setTimeout(() => {
-             animText3.innerHTML += `<span>${word[index]}</span>`;
-             typewriter4(txt4, index + 1);
-         }, tab2[index]);
-     }
- }
+type();
